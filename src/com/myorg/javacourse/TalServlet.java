d5aa8ee294b0aa9b57b832d36a3d0b5c0561ea65
takadosh <takadosh@gmail.com>
2015-04-01 20:10:43 +0300

    Ex 02

--- src/com/myorg/javacourse/TalServlet.java
+++ src/com/myorg/javacourse/TalServlet.java
@@ -1,11 +1,22 @@
 package com.myorg.javacourse;
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class TalServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		int num1;
+		int num2;
+		int num3;
+		
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		
+		int result = (num1+num2)*num3;
+		String resultStr = new String ("Result of ("+num1+"+"+num2+")*"+num3+"="+result+"");
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println(resultStr);
 	}
 }
